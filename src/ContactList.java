92c4b88495d00c71b6459a6cc32de40a85c10d8b
juliaim <julia.im@menloschool.org>
2024-01-18 18:21:00 -0800

    worked on everything but the last method

--- src/ContactList.java
+++ src/ContactList.java
@@ -11,15 +11,78 @@ public class ContactList {
     public void addContact(Person other) {
         contacts.add(other);
     }
-    public String printContacts() {
-        for (int i = 0; i < contacts.size(); i++) {
-            return contacts.get(i);
+    public void printContacts() {
+        for (Person person : contacts) {
+            System.out.println(person);
         }
     }
     public void sort(int sortBy) {
-        if (sortBy = 0) {
-
+        int size = contacts.size();
+        if (sortBy == 0) {
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = 0; j < size - 1 - i; i++) {
+                    if (contacts.get(j).getFirstName().compareTo(contacts.get(j+1).getFirstName()) > 0) {
+                        Person temp = contacts.get(j);
+                        contacts.set(j, contacts.get(j+1));
+                        contacts.set(contacts.get(j+1), temp);
+                    }
+                }
+            }
+        }
+        else if (sortBy == 0) {
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = 0; j < size - 1 - i; i++) {
+                    if (contacts.get(j).getLastName().compareTo(contacts.get(j+1).getLastName()) > 0) {
+                        Person temp = contacts.get(j);
+                        contacts.set(j, contacts.get(j+1));
+                        contacts.set(contacts.get(j+1), temp);
+                    }
+                }
+            }
+        }
+        else if (sortBy == 2) {
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = 0; j < size - 1 - i; i++) {
+                    if (contacts.get(j).getPhoneNumber().compareTo(contacts.get(j+1).getPhoneNumber()) > 0) {
+                        Person temp = contacts.get(j);
+                        contacts.set(j, contacts.get(j+1));
+                        contacts.set(contacts.get(j+1), temp);
+                    }
+                }
+            }
         }
     }
+    public Person searchByFirstName(String firstName) {
+        for (Person person : contacts) {
+            if (person.getFirstName().equals(firstName)) {
+                return person;
+            }
+        }
+        return null;
+    }
+    public Person searchByLastName(String lastName) {
+        for (Person person : contacts) {
+            if (person.getFirstName().equals(lastName)) {
+                return person;
+            }
+        }
+        return null;
+    }
+    public Person searchByPhoneNumber(String phoneNumber) {
+        for (Person person : contacts) {
+            if (person.getFirstName().equals(phoneNumber)) {
+                return person;
+            }
+        }
+        return null;
+    }
+    public String listStudents() {
+        for (Person person : contacts) {
+            if (person instanceof Student) {
+                return person;
+            }
+        }
+    }
+
 
 }
